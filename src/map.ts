bbc92ee74447afc321d11c086e5916bd47b67816
shamwoah <noahskinner5@gmail.com>
2023-02-04 10:06:58 -0500

    prevented us from getting slammed with fraud bc im a dumbass

--- src/map.ts
+++ src/map.ts
@@ -1,4 +1,36 @@
 import constants from './api_constants.json';
 
-let map: google.maps.Map;
-const centre: google.maps.LatLngLiteral = {lat:30, lng: -110};
+function initMap() {
+    const latLng = { lat: 43.26071490220645, lng: -79.91971892827242};
+
+    const map = new google.maps.Map(document.getElementById("map")!, {
+        zoom: 4,
+        center: latLng,
+    });
+
+    let infoWindow = new google.maps.InfoWindow({
+        content: "click for coords",
+        position: latLng,
+    });
+
+    infoWindow.open(map);
+
+    map.addListener("click", (mapsMouseEvent) => {
+        infoWindow.close();
+
+        infoWindow = new google.maps.InfoWindow({
+            position: mapsMouseEvent.latLng,
+        });
+        infoWindow.setContent(
+            JSON.stringify(mapsMouseEvent.latLng.toJSON(), null, 2)
+        );
+        infoWindow.open(map);
+    });
+}
+
+declare global {
+    interface Window {
+        initMap: () => void;
+    }
+}
+window.initMap = initMap;
