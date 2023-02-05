4e7cef2a8cf872f1f2e73034d75d0eaf34d0751c
shamwoah <noahskinner5@gmail.com>
2023-02-05 02:36:55 -0500

    website logic changes

--- apiCall_weather_real.ts
+++ apiCall_weather_real.ts
@@ -1,3 +1,7 @@
+import data from './api_constants.json' assert { type: 'JSON' };
+const token = data["openweather_api_key"];
+const {Loc} = require('./index.ts');
+
 interface Data {
   // The structure of the response data
   setresponse: JSON;
@@ -5,8 +9,7 @@ interface Data {
   age: number;
 }
 
-fetch('https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=a2bc44f0f6e89d7d610d239bdf435b37')
-
+fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${Loc.lat}&lon=${Loc.lng}&appid=${token}`)
   .then(response =>  {
     if (!response.ok) {
       throw new Error(response.statusText);
