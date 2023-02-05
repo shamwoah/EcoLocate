807eab1bcbe7e7b7412a5df6c2ffad6c0a85cb46
shamwoah <noahskinner5@gmail.com>
2023-02-04 21:37:23 -0500

    dynamically implemented staticMap

--- index.ts
+++ index.ts
@@ -1,5 +1,10 @@
+import data from './api_constants.json' assert { type: 'JSON' };
+const token = data["maps_sdk_key"];
+
 function initMap() {
     const myLatLng = { lat: 43.261, lng: -79.920};
+
+    var staticMap = new URL(`https://maps.googleapis.com/maps/api/staticmap?center=40,20&zoom=14&size=640x640&scale=2&format=png&maptype=satellite&key=${token}`);
 
     const map = new google.maps.Map(document.getElementById("map")!, {
         zoom: 4,
@@ -19,8 +24,8 @@ function initMap() {
         marker.setMap(null);
 
         var Location = {
-            lat: <number>mapsMouseEvent.latLng.toJSON()["lat"].toFixed(6),
-            lng: <number>mapsMouseEvent.latLng.toJSON()["lng"].toFixed(6),
+            lat: <number>mapsMouseEvent.latLng.toJSON()["lat"].toFixed(4),
+            lng: <number>mapsMouseEvent.latLng.toJSON()["lng"].toFixed(4),
         };
 
         marker = new google.maps.Marker({
@@ -28,12 +33,16 @@ function initMap() {
             position: mapsMouseEvent.latLng,
             title: Location.lat + ", " + Location.lng,
         });
+        
+        var apiImgTag = document.getElementById('staticmap')!;
+        staticMap = new URL(`https://maps.googleapis.com/maps/api/staticmap?center=${Location.lat},${Location.lng}&zoom=14&size=640x640&scale=2&format=png&maptype=satellite&key=AIzaSyDUmfFb_52uU1LA8wmWVWLS7veo8Wld3X4`);
+        apiImgTag.setAttribute("src", "staticMap.href");
     });
 }
 
 declare global {
     interface Window {
-        initMap: () => void;
+        initMap: () => void;    
     }
 }
 window.initMap = initMap;
--- staticAPIGet.ts
+++ staticAPIGet.ts
@@ -1,7 +1,7 @@
 import data from './api_constants.json' assert { type: 'JSON' };
 const token = data["maps_sdk_key"];
 const sig = data["maps_static_secret"]
-const {Loc} = require('./index.ts');
+const {Loc} = require('.');
 const Params = {
     coord: Loc.lat + "," + Loc.lng,
     zoom: '16',
@@ -13,9 +13,9 @@ const Params = {
 
 const staticMap = new URL(`https://maps.googleapis.com/maps/api/staticmap?center=40,20&zoom=14&size=640x640&scale=2&format=png&maptype=satellite&key=${token}`);
 
-const apiImgTag = document.createElement("img");
+var apiImgTag = document.getElementById("staticmap")!;
+apiImgTag.setAttribute("src", "staticMap.href");
 apiImgTag.setAttribute('alt', "static map");
 apiImgTag.setAttribute("width", "640");
 apiImgTag.setAttribute("height", "640");
-apiImgTag.setAttribute("src", staticMap.href);
-document.body.appendChild(apiImgTag);
+document.head.appendChild(apiImgTag);
