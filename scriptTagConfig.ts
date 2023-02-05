dcb446f357dbaa66aeabb99606617fed97d2b80f
shamwoah <noahskinner5@gmail.com>
2023-02-04 20:52:29 -0500

    added several web logic pages

--- scriptTagConfig.ts
+++ scriptTagConfig.ts
@@ -1,10 +1,8 @@
 import data from './api_constants.json' assert { type: 'JSON' };
-var token = data["maps_sdk_key"];
+const token = data["maps_sdk_key"];
+
+const mapCall = new URL(`https://maps.googleapis.com/maps/api/js?key=${token}&callback=initMap&v=weekly`);
 
 const apiScriptTag = document.createElement("script");
-
-const staticMapCall = new URL(`https://maps.googleapis.com/maps/api/js?key=${token}&callback=initMap&v=weekly`);
-
-apiScriptTag.setAttribute("src", staticMapCall.href);
-
+apiScriptTag.setAttribute("src", mapCall.href);
 document.body.appendChild(apiScriptTag);
--- staticAPIGet.ts
+++ staticAPIGet.ts
@@ -1,18 +1,21 @@
 import data from './api_constants.json' assert { type: 'JSON' };
-const key = data["maps_sdk_key"];
+const token = data["maps_sdk_key"];
+const sig = data["maps_static_secret"]
 const {Loc} = require('./index.ts');
 const Params = {
     coord: Loc.lat + "," + Loc.lng,
-    zoom: '12',
+    zoom: '16',
     size: '640x640',
     scale: '2',
     format: 'png',
     maptype: 'satellite',
 }
 
-const url = new URL(`https://maps.googleapis.com/maps/api/staticmap?center=${Params.coord}&zoom=${Params.zoom}&size=${Params.size}&scale=${Params.scale}&format=${Params.format}&maptype=${Params.maptype}&key=${key}`);
+const staticMap = new URL(`https://maps.googleapis.com/maps/api/staticmap?center=40,20&zoom=14&size=640x640&scale=2&format=png&maptype=satellite&key=${token}`);
 
-async function getImg(url) {
-    const response = await fetch(url);
-}
-
+const apiImgTag = document.createElement("img");
+apiImgTag.setAttribute('alt', "static map");
+apiImgTag.setAttribute("width", "640");
+apiImgTag.setAttribute("height", "640");
+apiImgTag.setAttribute("src", staticMap.href);
+document.body.appendChild(apiImgTag);
